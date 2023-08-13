ed1de3b4cb6203edb1557c7df49470771e5f5182
brandyn1234 <41130732+Brandyn1234@users.noreply.github.com>
2023-08-13 15:33:36 -0400

    created playRound function, change player variable to be lowercase

--- foundations/rock-paper-scissors/script.js
+++ foundations/rock-paper-scissors/script.js
@@ -1,4 +1,26 @@
+function getComputerChoice() {
+    let computerChoice = options[(Math.floor(Math.random() * options.length))];
+    return computerChoice;
+}
+
+function playRound(playerChoice, computerChoice) {
+    if (playerChoice === "rock") {
+        if (computerChoice === "rock") {
+            return (`You: ${playerChoice}\nComputer: ${computerChoice}\nDraw! Try Again!`);
+        }
+        else if (computerChoice === "paper") {
+            return (`You: ${playerChoice}\nComputer: ${computerChoice}\nYou Lose! Try Again!`);
+        }
+        else if (computerChoice === "scissors") {
+            return (`You: ${playerChoice}\nComputer: ${computerChoice}\nYou Win!`);
+        }
+    }
+    
+}
+
+const options = ["rock", "paper", "scissors"];
 let playerChoice = prompt("Rock, paper, or scissors?");
-var options = ["rock", "paper", "scissors"];
-let computerChoice = options[(Math.floor(Math.random() * options.length))];
+let playerLowerCase = playerChoice.toLowerCase();
+let computerChoice = getComputerChoice();
 console.log(computerChoice);
+console.log(playRound(playerChoice, computerChoice));
