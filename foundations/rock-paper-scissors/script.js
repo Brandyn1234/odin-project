da765a5b45b2f19db92edcd63041a664ffc7f678
brandyn1234 <41130732+Brandyn1234@users.noreply.github.com>
2023-08-13 16:42:46 -0400

    Added logic for checking other conditions paper/scissors to playRound function

--- foundations/rock-paper-scissors/script.js
+++ foundations/rock-paper-scissors/script.js
@@ -16,11 +16,33 @@ function playRound(playerChoice, computerChoice) {
         }
     }
     
+    if (playerChoice === "paper") {
+        if (computerChoice === "rock") {
+            return (`You: ${playerChoice}\nComputer: ${computerChoice}\nYou Win!`);
+        }
+        else if (computerChoice === "paper") {
+            return (`You: ${playerChoice}\nComputer: ${computerChoice}\nDraw! Try Again!`);
+        }
+        else if (computerChoice === "scissors") {
+            return (`You: ${playerChoice}\nComputer: ${computerChoice}\nYou Lose! Try Again!`);
+        }
+    }
+
+    if (playerChoice === "scissors") {
+        if (computerChoice === "rock") {
+            return (`You: ${playerChoice}\nComputer: ${computerChoice}\nYou Lose! Try Again!`);
+        }
+        else if (computerChoice === "paper") {
+            return (`You: ${playerChoice}\nComputer: ${computerChoice}\nYou Win!`);
+        }
+        else if (computerChoice === "scissors") {
+            return (`You: ${playerChoice}\nComputer: ${computerChoice}\nDraw! Try Again!`);
+        }
+    }
 }
 
 const options = ["rock", "paper", "scissors"];
 let playerChoice = prompt("Rock, paper, or scissors?");
 let playerLowerCase = playerChoice.toLowerCase();
 let computerChoice = getComputerChoice();
-console.log(computerChoice);
-console.log(playRound(playerChoice, computerChoice));
+console.log(playRound(playerLowerCase, computerChoice));
