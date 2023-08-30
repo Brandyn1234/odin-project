7fb9022bc1982c79e618cd8fa2b28cbc57b17f4d
brandyn1234 <41130732+Brandyn1234@users.noreply.github.com>
2023-08-30 08:29:09 -0400

    added score and footer content

--- foundations/rock-paper-scissors/script.js
+++ foundations/rock-paper-scissors/script.js
@@ -19,56 +19,63 @@ function Game() {
 
 
 
-    const img = document.querySelectorAll('.image-container');
-    img.forEach(imgs => {
-    imgs.addEventListener('click', (e)=>{
-    let playerchoice = e.target.id;
-
-    getComputerChoice = () => {
-        const options = ["rock", "paper", "scissors"];
-        let computerChoice = options[(Math.floor(Math.random() * options.length))];
-        return computerChoice;
-    }
-
-    let computerchoice = getComputerChoice();
-
-    // Game Logic //
-    console.log(computerchoice);
-    if (playerchoice === "rock") {
-        if (computerchoice === "rock") {
-            span.textContent = ++playerscore;
-            cpuspan.textContent = ++computerscore;
-        }
-
-        else if (computerchoice === "paper") {
-            span.textContent = ++playerscore;
-            cpuspan.textContent = ++computerscore;
-        }
-
-        else if (computerchoice === "scissors") {
-            span.textContent = ++playerscore;
-            cpuspan.textContent = ++computerscore;
-        }
-    }
+    updateScreen = () => {
+        const img = document.querySelectorAll('.image-container');
+        img.forEach(imgs => {
+        imgs.addEventListener('click', (e)=>{
+        let playerchoice = e.target.id;
     
-    if (playerchoice === "paper") {
-        /* To-do placeholder text*/ 
+        getComputerChoice = () => {
+            const options = ["rock", "paper", "scissors"];
+            let computerChoice = options[(Math.floor(Math.random() * options.length))];
+            return computerChoice;
+        }
+    
+        let computerchoice = getComputerChoice();
+        
+        // Game Logic //
+        console.log(computerchoice);
+        if (playerchoice === "rock") {
+            if (computerchoice === "rock") {
+                span.textContent = ++playerscore;
+                cpuspan.textContent = ++computerscore;
+            }
+    
+            else if (computerchoice === "paper") {
+                span.textContent = ++playerscore;
+                cpuspan.textContent = ++computerscore;
+            }
+    
+            else if (computerchoice === "scissors") {
+                span.textContent = ++playerscore;
+                cpuspan.textContent = ++computerscore;
+            }
+        }
+        
+        if (playerchoice === "paper") {
+            /* To-do placeholder text*/ 
+        }
+    
+        if (playerchoice === "scissors") {
+            /* to-do placeholder text */
+        }
+
+        if (playerscore > 5 || computerscore > 5) {
+            alert("Game Over");
+            playerscore = 0;
+            span.textContent = playerscore;
+            computerscore = 0;
+            cpuspan.textContent = computerscore; 
+        }
+    
+        
+    
+        });  
+        });
+
     }
 
-    if (playerchoice === "scissors") {
-        /* to-do placeholder text */
-    }
-
-    if (playerscore > 5 || computerscore > 5) {
-        alert("Game Over");
-        playerscore = 0;
-        span.textContent = playerscore;
-        computerscore = 0;
-        cpuspan.textContent = computerscore;
-    }
-
-    });  
-    });
+    updateScreen();
 }
 
 Game();
