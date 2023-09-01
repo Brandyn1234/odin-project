6795914ed5e59461a77043c694fc2f78224ddb7a
brandyn1234 <41130732+Brandyn1234@users.noreply.github.com>
2023-09-01 00:36:11 -0400

    finished up ui for rock paper scissors

--- foundations/rock-paper-scissors/script.js
+++ foundations/rock-paper-scissors/script.js
@@ -6,8 +6,6 @@ function Game() {
     const span = document.createElement('span');
     span.textContent = playerscore;
     playercontainer.appendChild(span);
-    console.log(playercontainer);
-    // testing code above //
 
     // computer score //
     let computerscore = 0;
@@ -16,15 +14,12 @@ function Game() {
     cpuspan.textContent = computerscore;
     computercontainer.appendChild(cpuspan);
 
-
-
-
-    updateScreen = () => {
+    eventListener = () => {
         const img = document.querySelectorAll('.image-container');
         img.forEach(imgs => {
         imgs.addEventListener('click', (e)=>{
         let playerchoice = e.target.id;
-    
+
         getComputerChoice = () => {
             const options = ["rock", "paper", "scissors"];
             let computerChoice = options[(Math.floor(Math.random() * options.length))];
@@ -35,47 +30,78 @@ function Game() {
         
         // Game Logic //
         console.log(computerchoice);
+    
         if (playerchoice === "rock") {
             if (computerchoice === "rock") {
-                span.textContent = ++playerscore;
-                cpuspan.textContent = ++computerscore;
+                document.getElementById("demo").textContent = "It's a tie! You both chose rock";
             }
     
             else if (computerchoice === "paper") {
-                span.textContent = ++playerscore;
                 cpuspan.textContent = ++computerscore;
+                document.getElementById("demo").textContent = "You lose! You chose rock, computer chose paper";
             }
     
             else if (computerchoice === "scissors") {
                 span.textContent = ++playerscore;
-                cpuspan.textContent = ++computerscore;
+                document.getElementById("demo").textContent = "You win! You chose rock, computer chose scissors";
             }
         }
         
         if (playerchoice === "paper") {
-            /* To-do placeholder text*/ 
+            if (computerchoice === "rock") {
+                span.textContent = ++playerscore;
+                document.getElementById("demo").textContent = "You win! You chose paper, computer chose rock";
+            }
+    
+            else if (computerchoice === "paper") {
+                document.getElementById("demo").textContent = "It's a tie! You both chose paper";
+            }
+    
+            else if (computerchoice === "scissors") {
+                cpuspan.textContent = ++computerscore;
+                document.getElementById("demo").textContent = "You lose! You chose paper, computer chose scissors";
+            }
         }
     
         if (playerchoice === "scissors") {
-            /* to-do placeholder text */
+            if (computerchoice === "rock") {
+                cpuspan.textContent = ++computerscore;
+                document.getElementById("demo").textContent = "You lose! You chose scissors, computer chose rock";
+            }
+    
+            else if (computerchoice === "paper") {
+                span.textContent = ++playerscore;
+                document.getElementById("demo").textContent = "You win! You chose scissors, computer chose paper";
+            }
+    
+            else if (computerchoice === "scissors") {
+                document.getElementById("demo").textContent = "It's a tie! You both chose scissors";
+            }
         }
 
-        if (playerscore > 5 || computerscore > 5) {
-            alert("Game Over");
+        if (playerscore >= 5) {
+            document.getElementById("demo").textContent = "You win! Play Again?";
             playerscore = 0;
             span.textContent = playerscore;
             computerscore = 0;
-            cpuspan.textContent = computerscore; 
+            cpuspan.textContent = computerscore;
         }
-    
+
+        if (computerscore >= 5) {
+            document.getElementById("demo").textContent = "You lose! Play Again?";
+            playerscore = 0;
+            span.textContent = playerscore;
+            computerscore = 0;
+            cpuspan.textContent = computerscore;
+        }
         
-    
+        
         });  
         });
 
     }
 
-    updateScreen();
+    eventListener();
 }
 
 Game();
