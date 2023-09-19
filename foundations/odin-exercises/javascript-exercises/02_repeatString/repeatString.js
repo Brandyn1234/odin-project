1fd911c4cb701474ad78be9b2e6cc95c663b01ef
Brandyn1234 <41130732+Brandyn1234@users.noreply.github.com>
2023-09-19 02:37:06 -0500

    finished solution for repeatString tdd exercise

--- foundations/odin-exercises/javascript-exercises/02_repeatString/repeatString.js
+++ foundations/odin-exercises/javascript-exercises/02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) return "ERROR";
+    let multiString = "";
+    for(let i = 0; i < num; i++) {
+        multiString += string;
+    }
+    return multiString;
 };
 
 module.exports = repeatString;
