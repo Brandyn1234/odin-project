2b0f0def1a6ba4f952c4b4b6b4fbb0e2009d2b1d
Brandyn1234 <41130732+Brandyn1234@users.noreply.github.com>
2023-09-19 21:52:01 -0500

    finished the function to sum numbers - passes all tests cases

--- foundations/odin-exercises/javascript-exercises/05_sumAll/sumAll.js
+++ foundations/odin-exercises/javascript-exercises/05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+    let sum = 0;
 
+    if (startNum < 0 || endNum < 0) return "ERROR";
+    if (!Number.isInteger(startNum) || !Number.isInteger(endNum)) return "ERROR";
+
+    for (i=startNum; i < endNum+1; i++) {
+        sum += i;
+    }
+    for(i=startNum; i > endNum-1; i--) {
+        sum += i;
+    }
+    
+    return sum;
 };
-
+console.log(sumAll(1, 4));
 module.exports = sumAll;
