d6ff79208a287a213fb4d30cf9712563e7bb12e9
Brandyn1234 <41130732+Brandyn1234@users.noreply.github.com>
2023-09-26 06:08:48 -0500

    added some css styling/setup to add the js

---
+++ foundations/etch-a-sketch/main.js
@@ -0,0 +1,9 @@
+function createBoard() {
+    let board = document.querySelector('.board');
+    board.style.gridTemplateColumns = "repeat(16, 1fr)";
+    board.style.gridTemplateRows = "repeat(16, 1fr)";
+    console.log(board);
+}
+
+createBoard();
+
