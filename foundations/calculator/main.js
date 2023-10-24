1575236e4abcbd5d44003e853fc278f45d143e73
Brandyn1234 <41130732+Brandyn1234@users.noreply.github.com>
2023-10-24 13:17:28 -0500

    added numbers to display on screen when clicking button

--- foundations/calculator/main.js
+++ foundations/calculator/main.js
@@ -2,6 +2,26 @@ let firstOperand = '';
 let secondOperand = '';
 let operator = '';
 
+const screenData = document.getElementById('screen-data');
+const numberButtons = document.querySelectorAll('.numbers');
+const operationButtons = document.querySelectorAll('.operations'); 
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        if (screenData.innerText === '0') {
+            screenData.textContent = '';
+        };
+        screenData.textContent += button.textContent
+    });
+});
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        operator = button.textContent;
+        screenData.textContent += button.textContent
+    });
+});
+
 function add(a, b){
     return a + b;
 };
@@ -33,7 +53,6 @@ function operate(operator, a, b){
     };
 };
 
-let result = operate('/', 16, 4);
-console.log(result);
+
 
 
